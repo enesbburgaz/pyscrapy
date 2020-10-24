8ad08f1bcabb459b90512053067dffd9295ee73d
enesbburgaz <enesbburgaz@gmail.com>
2020-10-24 03:19:01 +0300

    updated

--- pyscrapy/spiders/pyscrapy2.py
+++ pyscrapy/spiders/pyscrapy2.py
@@ -9,27 +9,25 @@ from urllib.parse import urlparse
 class Pyscrapy2Spider(scrapy.Spider):
     name = 'pyscrapy2'
     start_urls = ['link']
-
+    
     def parse(self, response):
         for href in response.xpath("//*[@id='search-app']/div/div[2]/div[2]/div[2]/div/div[@class='p-card-wrppr']/div[1]/a/@href"):
             url = response.urljoin(href.extract())
             yield scrapy.Request(url, callback=self.parsePage)
-
-        """
-        #sayfa sayisi   
+  
         count= range(2,11)
         for x in count:
             nextPage = '/cep-telefonu?pi={0}'.format(x)
             url = response.urljoin(nextPage)
             yield scrapy.Request(url, self.parse)
-        """
+        
 
     def parsePage(self, response):
         yield scrapy.Request(response.url+'/yorumlar', callback=self.getComments)
 
     def getComments(self, response):
-        productBrand = response.xpath('//*[@id="rating-and-review-app"]/div/div[1]/div/div[1]/div[2]/h1/span[1]/text()')
-        productName = response.xpath('//*[@id="rating-and-review-app"]/div/div[1]/div/div[1]/div[2]/h1/span[2]/text()')
+        productBrand = response.xpath('//*[@id="rating-and-review-app"]/div/div[1]/div/div[1]/div[2]/h1/span[1]/text()').extract()
+        productName = response.xpath('//*[@id="rating-and-review-app"]/div/div[1]/div/div[1]/div[2]/h1/span[2]/text()').extract()
         userName = response.xpath('//*[@id="rating-and-review-app"]/div/div[2]/div/div[2]/div[3]/div[2]/div[@class="rnr-com-w"]/div[2]/div[1]/span[1]/text()[1]').extract()
         comments= response.xpath('//*[@id="rating-and-review-app"]/div/div[2]/div/div[2]/div[3]/div[2]/div[@class="rnr-com-w"]/div[1]/div/text()').extract()
         buyInfo = response.xpath('//*[@id="rating-and-review-app"]/div/div[2]/div/div[2]/div[3]/div[2]/div[@class="rnr-com-w"]/div[2]/div[1]/span[2]/span/text()').extract()        
